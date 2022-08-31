47b18909b51753e0b31a234f42f8013edeba2b8e
minhvu278 <33751963+minhvu278@users.noreply.github.com>
2022-08-31 23:48:08 +0700

    Revert "add store menu"

--- resources/views/admin/menus/create.blade.php
+++ resources/views/admin/menus/create.blade.php
@@ -31,9 +31,9 @@
                             </div>
                         </div>
                         <div class="sm:col-span-6">
-                            <label for="price" class="block text-sm font-medium text-gray-700"> Price </label>
+                            <label for="name" class="block text-sm font-medium text-gray-700"> Price </label>
                             <div class="mt-1">
-                                <input min="0.00" max="10000.00" type="number" id="price" name="price"
+                                <input type="number" id="price" name="price"
                                        class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5 @error('name') border-red-400 @enderror"/>
                             </div>
                         </div>
@@ -47,8 +47,7 @@
                         <div class="sm:col-span-6 pt-5">
                             <label for="body" class="block text-sm font-medium text-gray-700">Categories</label>
                             <div class="mt-1">
-                                <select id="categories" name="categories[]" class="form-multiselect block w-full mt-1"
-                                    multiple>
+                                <select multiple>
                                     @foreach($categories as $category)
                                         <option>{{ $category->name }}</option>
                                     @endforeach
