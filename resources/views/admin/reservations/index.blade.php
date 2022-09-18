030e5d1c06871905964dc233037cadd6880224d6
minhvu278 <dominhvu278@gmail.com>
2022-09-18 23:18:59 +0700

    done reservation

--- resources/views/admin/reservations/index.blade.php
+++ resources/views/admin/reservations/index.blade.php
@@ -16,62 +16,54 @@
                     <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
                         <th scope="col" class="py-3 px-6">
-                            Product name
+                            First Name
                         </th>
                         <th scope="col" class="py-3 px-6">
-                            Color
+                            Last Name
                         </th>
                         <th scope="col" class="py-3 px-6">
-                            Category
+                            Email
                         </th>
                         <th scope="col" class="py-3 px-6">
-                            Price
+                            Phone
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Reservation Date
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Table
+                        </th>
+                        <th scope="col" class="py-3 px-6">
+                            Guest Number
                         </th>
                     </tr>
                     </thead>
                     <tbody>
+                    @foreach($reservations as $reservation)
                     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Apple MacBook Pro 17"
+                            {{ $reservation->first_name }}
                         </th>
-                        <td class="py-4 px-6">
-                            Sliver
-                        </td>
-                        <td class="py-4 px-6">
-                            Laptop
-                        </td>
-                        <td class="py-4 px-6">
-                            $2999
-                        </td>
-                    </tr>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                         <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Microsoft Surface Pro
+                            {{ $reservation->last_name }}
                         </th>
-                        <td class="py-4 px-6">
-                            White
-                        </td>
-                        <td class="py-4 px-6">
-                            Laptop PC
-                        </td>
-                        <td class="py-4 px-6">
-                            $1999
-                        </td>
-                    </tr>
-                    <tr class="bg-white dark:bg-gray-800">
                         <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                            Magic Mouse 2
+                            {{ $reservation->email }}
                         </th>
-                        <td class="py-4 px-6">
-                            Black
-                        </td>
-                        <td class="py-4 px-6">
-                            Accessories
-                        </td>
-                        <td class="py-4 px-6">
-                            $99
-                        </td>
+                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $reservation->phone }}
+                        </th>
+                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $reservation->res_date }}
+                        </th>
+                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $reservation->table_id }}
+                        </th>
+                        <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                            {{ $reservation->guest_number }}
+                        </th>
                     </tr>
+                    @endforeach
                     </tbody>
                 </table>
             </div>
