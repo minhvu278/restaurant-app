ec90ea24594eaefe1a3980ce84b6be64034756dc
minhvu278 <dominhvu278@gmail.com>
2022-09-20 15:25:33 +0700

    add rule datebetween

--- app/Http/Requests/ReservationStoreRequest.php
+++ app/Http/Requests/ReservationStoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Rules\DateBetween;
 use Illuminate\Foundation\Http\FormRequest;
 
 class ReservationStoreRequest extends FormRequest
@@ -27,7 +28,7 @@ class ReservationStoreRequest extends FormRequest
             'first_name' => 'required',
             'last_name' => 'required',
             'email' => 'required',
-            'res_date' => 'required',
+            'res_date' => ['required', 'date', new DateBetween],
             'phone' => 'required',
             'table_id' => 'required',
             'guest_number' => 'required',
